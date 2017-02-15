aa54067364958e30fd3be0b5c18b4e3fd947c4a8
a33eyes <ariiigu@gmail.com>
2017-02-15 18:47:17 +0000

    Updated routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'static_pages/about'
 
-  resources :games
-  resources :users
+  resources :users do
+    resources :games
+  end
+
+#  resources :games
+#  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
